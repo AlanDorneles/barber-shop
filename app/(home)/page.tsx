cb260f5dcef952ee93680f982abab93078dd976d
Alan Dorneles <alanricardo.97@gmail.com>
2024-01-30 16:14:06 -0300

    footer

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -6,6 +6,7 @@ import BookingItem from "../components/booking-item";
 import { db } from "../_lib/prisma";
 import BarbershopItem from "./_components/barbershop-item";
 
+
 export default async function Home() {
   const barbershop = await db.barbershop.findMany({})
   return (
@@ -37,6 +38,19 @@ export default async function Home() {
           </div>
 
         </div>
+        <div className="mt-5">
+          <h2 className='text-sm mb-3 uppercase text-gray-400 font-bold'>Populares</h2>
+          <div  className="flex gap-2 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+            {barbershop.map((barbershop) => (
+              <BarbershopItem key={barbershop.id} barbershop={barbershop}/>
+
+            ))}
+            <h1>{barbershop.name}</h1>
+
+          </div>
+
+        </div>
+        
         
       </div>
     </div>
---
+++ app/components/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return ( 
+        <div className="flex justify-center items-center w-full bg-secondary py-6 px-5">
+            <p className="text-gray-400">@2023 Copyright Barber-Shop</p>
+        </div>
+    );
+}
+ 
+export default Footer;
