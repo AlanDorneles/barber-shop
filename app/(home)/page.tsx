45e916dd34660f31957864ca17bc387e6a1eb5e8
Alan Dorneles <alanricardo.97@gmail.com>
2024-01-30 14:53:39 -0300

    feat: card da barbearia

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,6 +2,7 @@ import { Header } from "../components/ui/header";
 import { format } from "date-fns";
 import { ptBR } from "date-fns/locale";
 import Search from "./_components/search";
+import BookingItem from "../components/booking-item";
 
 export default function Home() {
   return (
@@ -17,6 +18,11 @@ export default function Home() {
           </p>
         </div>
         <Search/>
+        <div className="mt-5">
+          <h2 className='text-sm mb-3 uppercase text-gray-400 font-bold'>Agendamentos</h2>
+          <BookingItem />
+        </div>
+        
       </div>
     </div>
   );
