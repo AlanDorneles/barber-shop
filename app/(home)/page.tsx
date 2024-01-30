f4f935a115a416dfd99e4b76d55dec3b5039a6fb
Alan Dorneles <alanricardo.97@gmail.com>
2024-01-30 13:33:25 -0300

    feat: mensagem de boas vindas

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,8 +1,18 @@
 import { Header } from "../components/ui/header";
+import {format} from 'date-fns'
+import { ptBR } from "date-fns/locale";
 
 
 export default function Home() {
   return (
-    <div><Header/></div>
+    <div>
+      <Header/>
+      <div className="px-5 pt-5">
+      <h2 className="text-xl font-bold">Olá, Alan</h2>
+      <p className="capitalize text-sm">{format(new Date(), "EEEE' ,' d 'de ' MMM", {
+        locale:ptBR,
+      })}</p>
+      </div>
+    </div>
   );
 }
